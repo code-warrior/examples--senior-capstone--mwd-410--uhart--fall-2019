412256b9cf4029502d1ef16f1ce397489dfd5ea6
Roy A Vanegas <roy@thecodeeducators.com>
2019-10-16 10:42:02 -0400

    Add a slightly modified version of the gulpfile.js file we saw in class today

---
+++ 10-16-2019/gulp-project/gulpfile.js
@@ -0,0 +1,36 @@
+const { src, dest, watch, series } = require(`gulp`);
+const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
+
+let validateHTML = () => {
+    return src(`html/*.html`)
+        .pipe(htmlValidator());
+};
+
+let compressHTML = () => {
+    return src(`html/*.html`)
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`app/`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 0, // A delay is sometimes helpful when reloading at the
+        server: {       // end of a series of tasks.
+            baseDir: [
+                `temp`,
+                `html`
+            ]
+        }
+    });
+
+    watch(`html/**/*.html`, series(validateHTML)).on(`change`, reload);
+};
+
+exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
+exports.build = series(validateHTML, compressHTML);
+exports.serve = series(validateHTML, serve);
